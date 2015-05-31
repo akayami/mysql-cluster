19c43af127e0e4e4410c514a69f8b0a84a296fe2
Tomasz Rakowski <tomasz@akayami.com>
2015-05-31 14:23:29 -0400

    Added Update helper.

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
  * New node file
  */
 
-var mysql = require('mysql');
+var mysql = require('mysql2');
 var merge = require('merge');
 var assert =  require('assert');
 var expect = require('chai').expect;
@@ -50,9 +50,9 @@ var config = {
 
 var mysql = require('mysql');
 
-describe("MySQL Cluster", function() {	
+describe("MySQL Cluster", function() {
 	var serviceConn;
-	
+
 	before(function(done) {
 		serviceConn = mysql.createConnection({
 			host : 'localhost',
@@ -71,63 +71,63 @@ describe("MySQL Cluster", function() {
 	before(function(done) {
 		serviceConn.query('create database ??', [testDbName], function(err, result) {
 			if (!err) {
-				done();				
+				done();
 			} else {
 				done(err);
 			}
 		});
 	});
-	
+
 	before(function(done) {
 		serviceConn.query('use ??', [testDbName], function(err, result) {
 			if (!err) {
-				done();				
+				done();
 			} else {
 				done(err);
 			}
 		});
 	});
-	
+
 	before(function(done) {
 		serviceConn.query('GRANT ALL ON ??.* TO ?@? IDENTIFIED BY ?', [testDbName, 'write', 'localhost', ''], function(err, result) {
 			if (!err) {
-				done();				
+				done();
 			} else {
 				done(err);
 			}
 		});
 	});
-	
+
 	before(function(done) {
 		serviceConn.query('GRANT SELECT ON ??.* TO ?@? IDENTIFIED BY ?', [testDbName, 'readonly', 'localhost', ''], function(err, result) {
 			if (!err) {
-				done();				
+				done();
 			} else {
 				done(err);
 			}
 		});
 	});
-	
+
 	after(function(done) {
 		serviceConn.query('REVOKE ALL ON ??.* FROM ?@localhost', [testDbName, 'write'], function(err, result) {
 			if (!err) {
-				done();				
+				done();
 			} else {
 				done(err);
 			}
 		});
 	});
-	
+
 	after(function(done) {
 		serviceConn.query('REVOKE SELECT ON ??.* FROM ?@localhost', [testDbName, 'readonly'], function(err, result) {
 			if (!err) {
-				done();				
+				done();
 			} else {
 				done(err);
 			}
 		});
 	});
-	
+
 	after(function(done) {
 		serviceConn.query('drop database ??', [testDbName], function(err, result) {
 			if(!err) {
@@ -137,15 +137,15 @@ describe("MySQL Cluster", function() {
 			}
 		});
 	});
-	
-	
-	
+
+
+
 	var cluster;
 	it('should instanciate a cluster defined by config', function() {
 		cluster = require("../lib/index.js")(config);
 		expect(cluster).to.be.an('object');
 	});
-	
+
 	var master;
 	it("should be able to aquire master connection using master()", function(done) {
 		cluster.master(function(err, conn) {
@@ -157,8 +157,8 @@ describe("MySQL Cluster", function() {
 			}
 		});
 	});
-	
-	
+
+
 	var slave;
 	it("should be able to aquire slave connection using slave()", function(done) {
 		cluster.slave(function(err, conn) {
@@ -170,7 +170,7 @@ describe("MySQL Cluster", function() {
 			}
 		})
 	})
-	
+
 	it('should be able to create a table using master connection', function(done) {
 		master.query('CREATE TABLE ?? (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,`test` VARCHAR(45) NULL, PRIMARY KEY (`id`));', ['test'], function(err, result) {
 			if(err) {
@@ -180,7 +180,7 @@ describe("MySQL Cluster", function() {
 			}
 		});
 	});
-	
+
 	it('should NOT be able to create a table using slave connection', function(done) {
 		slave.query('CREATE TABLE ?? (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,`test` VARCHAR(45) NULL, PRIMARY KEY (`id`));', ['test_slave'], function(err, result) {
 			if(err) {
@@ -190,7 +190,7 @@ describe("MySQL Cluster", function() {
 			}
 		});
 	});
-	
+
 	it('should be able to write into table', function(done) {
 		master.query('INSERT INTO ?? SET `test`=?', ['test', 'hello'], function(err, result) {
 			if(err) {
@@ -200,7 +200,7 @@ describe("MySQL Cluster", function() {
 			}
 		});
 	});
-	
+
 	it('should be able to write into table using shorcut insert method', function(done) {
 		master.insert('test', {test: 'hello'}, function(err, result) {
 			if(err) {
@@ -212,7 +212,7 @@ describe("MySQL Cluster", function() {
 			}
 		});
 	});
-	
+
 	it('should NOT be able to write into table usign slave connection', function(done) {
 		slave.query('INSERT INTO ?? SET `test`=?', ['test', 'hello'], function(err, result) {
 			if(err) {
@@ -222,8 +222,28 @@ describe("MySQL Cluster", function() {
 			}
 		});
 	});
-	
-	
+
+	it('should update a field', function(done) {
+		master.update('test', {test: 'test updated'}, 'ID = ?', [1], function(err, result) {
+			if(err) {
+				done(err);
+			} else {
+				done();
+			}
+		})
+	});
+
+	it('should be able to read from table using named placeholders', function(done) {
+		master.config.namedPlaceholders = true;
+		master.execute('select * from test where id = :id', {table: 'test', id: 1}, function(err, result) {
+			if(err) {
+				done(err);
+			} else {
+				done();
+			}
+		});
+	});
+
 	it('should be able to read from table', function(done) {
 		master.query('select * from ?? where id=1', ['test'], function(err, result) {
 			if(err) {
@@ -242,7 +262,7 @@ describe("MySQL Cluster", function() {
 			}
 		});
 	});
-	
+
 	it('should not be able to read from a dropped table', function(done) {
 		master.query('select * from ?? where id=1', ['test'], function(err, result) {
 			if(err) {
